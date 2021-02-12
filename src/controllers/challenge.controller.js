e1258295812a86048c0637e26a617aed22d77fc0
Ben Winchester <benjamin.m.winchester@googlemail.com>
2021-02-12 10:55:43 -0500

    Enhancement: #11 GET Challenge by Difficult (#26)
    * Enhancement: #11 GET Challenge by Difficult
    __ISSUE:__
    User requires ability to add query parameters to limit difficulty of
    randomly selected challenge to a given difficulty level
    
    __DESCRIPTION:__
    Pool of possible questions for random selection are filtered on passed
    "level" query parameter
    
    ___STEPS TO TEST:__
    
    Postman
    
    * Enhancement: Get Challenge By Difficult - Add Tests
    
    __ISSUE:__
    Closes #11
    
    __DESCRIPTION:__
    Adds Tests for Current Endpoints, and removes unused code
    
    ___STEPS TO TEST:__
    Passes Test coverave requirements
    
    * change variable name

--- src/controllers/challenge.controller.js
+++ src/controllers/challenge.controller.js
@@ -1,17 +1,18 @@
 // Mock repository
 const data = require("../../ex.json");
 
-const getRandomChallenge = (req, res) => {
-  const question = data[Math.floor(Math.random() * data.length)];
-  res.send(question);
-};
-
-const getChallengeById = (req, res) => {
-  const question = data.find((obj) => obj.id === req.params.id);
-  res.send(question);
+const getChallenge = (req, res) => {
+  let possibleQuestions = [...data];
+  if (req.query.level) {
+    possibleQuestions = possibleQuestions.filter(
+      (question) => question.level === parseInt(req.query.level)
+    );
+  }
+  const randomQuestion =
+    possibleQuestions[Math.floor(Math.random() * possibleQuestions.length)];
+  res.send(randomQuestion);
 };
 
 module.exports = {
-  getRandomChallenge,
-  getChallengeById,
+  getChallenge,
 };
